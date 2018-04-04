d15a657e44f4e9d40cf10ddd191c7ccd099cd1fd
Nathaniel Hoefer <nathaniel.d.hoefer@gmail.com>
2018-04-04 16:19:21 -0500

    Add: Argument validation

--- compile_errors.sh
+++ compile_errors.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 #
-# Compiles a list of errors from VL log files found in test.log stored in directories as errors.log
+# Compiles a list of errors from VL log files found in test.log stored in 
+# directories as errors.log
 #
 # Synopsis: compile_errors.sh [directory] [-r]
 #
-# [directory]	If directory provided, it will be the initial location for test.log search, otherwise the present working directory will be used. 
+# [directory]	If directory provided, it will be the initial location for 
+# 		test.log search, otherwise the present working directory will 
+# 		be used. 
 #
-# [-r]	Recursively compiles list of errors in each test case and a summary error log file as suite_errors.log in suite directory. Requires directory to be a test suite (indicated by name starting with 'Ts') and contains test case directories starting with 'Tc' which directly hold the test.log files.
+# [-r]	Recursively compiles list of errors in each test subdirectory and a 
+# 	generates summary error log file as error_summary.log in directory 
+# 	provided.
 
 
-#######################################
+################################################################################
 # Global Constants
-#######################################
-LOG_FILE_NAME=test.log
-ERROR_FILE_NAME=error.log
-SUITE_ERR_FILE_NAME=suite_error.log
+################################################################################
+LOG_FILE_NAME="test.log"
+ERROR_FILE_NAME="error.log"
+SUITE_ERR_FILE_NAME="suite_error.log"
+REC_FLAG="-r"
+INVALID_ARGS="Invalid arguments!"
 
 
-#######################################
-# Parse logs from the test.log file
-# and creates error.log file in pwd.
+################################################################################
+# Parse logs from the test.log file and creates error.log file in specifed
+# directory.
 #
 # Arguments:
-#	Directory containing the
-#       test.log file
+#	Directory containing the test.log file
 # Returns:
-#	error count	
-#######################################
+#	None
+################################################################################
 parseLogs () {
   error_count=0
   dir=$1
@@ -39,14 +45,14 @@ parseLogs () {
   # Ensure test.log exists
   if [ ! -e $log_path ]; then
     echo Test.log doesn\'t exist in this directory!
-    exit 1
+    return
   fi
 
   # Check if error.log file exists
   echo Compiling error report for $test_name
   if [ -s $err_path ]; then
     echo --- Error.log file already exists, skipping
-    exit 0
+    return
   fi 
 
   # Compile error report
@@ -65,12 +71,62 @@ parseLogs () {
   echo -e "Error Count: $error_count" >> $err_path
   echo -e "-------------------------------------------" >> $err_path
 
-  echo Compile completed for $test_name
-  exit 0
+  echo Compile completed for $test_name, Errors Found: $error_count
 }
 
-# Determine if recursive call
-parseLogs $1
+
+################################################################################
+# Recursively call parseLogs for each subdirectory starting with 'Ts' and 
+# generates the error_summary.log file in the specified directory.
+#
+# Arguments:
+#	Directory containing the test directories labelled with 'Ts'
+# Returns:
+#	None
+################################################################################
+parseTests() {
+  # TODO
+  echo Tests Parsed!
+}
 
 
+# Validate based on number of arguments and execute accordingly
+case $# in
+ 
+  # Default to use pwd to parse logs
+  0)
+    parseLogs $PWD ;;
+  
+  # Check for flag or dir and execute 
+  1)
+    if [ $1 = $REC_FLAG ]; then
+      parseTests $PWD
+    elif [ -d $1 ]; then
+      parseLogs $1
+    else
+      echo $INVALID_ARGS
+      exit 1
+    fi ;;
 
+  # Verify that one arg is flag and another is a dir 
+  2)
+    if [ $1 = $REC_FLAG ] || [ $2 = $REC_FLAG ]; then
+      if [ -d $1 ]; then
+        parseTests $1
+      elif [ -d $2 ]; then
+        parseTests $2
+      else
+        echo $INVALID_ARGS
+        exit 1
+      fi
+    else
+      echo $INVALID_ARGS
+      exit 1
+    fi ;;
+
+  # All other number of args are invalid
+  *)
+    echo $INVALID_ARGS
+    exit 1
+esac
+exit 0
